4f01ba20f46c90c1f67f143af8e0f7f969a8f863
abhinav84094 <abhinav84094@gmail.com>
2025-05-07 15:21:54 +0530

    Product Pages added to the home page

---
+++ Pages/everything/script.js
@@ -0,0 +1,45 @@
+const api = "https://fakestoreapi.com/products" ;
+
+var data;
+
+async function abc(){
+
+    try{
+        data = await fetch("https://fakestoreapi.com/products" )
+    .then(response =>{return response.json()})
+    }
+    catch(err){
+        console.log(err);
+    } 
+
+    console.log(data)
+
+    for(let i=0; i<data.length; i++){
+        console.log(data[i].category)
+
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+    
+        card_body.appendChild(card);
+    
+    
+        const img = document.createElement("img");
+        const title = document.createElement("h4");
+        const price = document.createElement("div"); 
+    
+        img.src = `${data[i].image}` ;
+        title.textContent = `${data[i].title}`;
+        price.textContent = `$ ${data[i].price}`;
+
+        img.classList.add("card_img");
+    
+        card.appendChild(img);
+        card.appendChild(title);
+        card.appendChild(price);
+    }
+
+}
+
+abc();
+
---
+++ Pages/women/script.js
@@ -0,0 +1,46 @@
+const api = "https://fakestoreapi.com/products" ;
+
+var data;
+
+async function abc(){
+
+    try{
+        data = await fetch("https://fakestoreapi.com/products" )
+    .then(response =>{return response.json()})
+    }
+    catch(err){
+        console.log(err);
+    } 
+
+    console.log(data)
+    
+
+    for(let i=0; i<data.length; i++){
+
+        
+
+        if(data[i].category == "women's clothing"){
+
+            const card = document.createElement("div");
+            card.classList.add("card");
+        
+            card_body.appendChild(card);
+        
+        
+            const img = document.createElement("img");
+            const h4 = document.createElement("h4"); 
+        
+            img.src = `${data[i].image}` ;
+            h4.textContent = `${data[i].title}`;
+            img.classList.add("card_img");
+        
+            card.appendChild(img);
+            card.appendChild(h4)
+        }
+
+    }
+
+}
+
+abc();
+
